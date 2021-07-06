265373a8b9b902313ff9e52d0bd102a1a4ed8cea
Anders Lund <anderslund@Anderss-MacBook-Pro.local>
2021-07-06 16:38:19 -0600

    added query flagged endpoint

--- routes/moderation.js
+++ routes/moderation.js
@@ -1,3 +1,15 @@
 const router = require('express').Router();
 
+const StreamChat = require('stream-chat').StreamChat;
+const chatClient = StreamChat.getInstance(process.env.API_KEY, process.env.API_SECRET);
+
+router.route('/flagged').post((req, res) => {
+    const { filter, options } = req.body;
+    chatClient.queryMessageFlags(filter, options).then((flaggedMessages) => {
+        res.status(200).send(JSON.stringify(flaggedMessages.flags));
+    }).catch((err) => {
+        res.status(401).send(`[ERROR]: ${err}`);
+    });
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -2,12 +2,14 @@ require('dotenv').config()
 
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
+app.use(bodyParser.json());
 
 //routes
 const moderationRouter = require('./routes/moderation');
